0ddcf1c79ba76bf26cd809c54a932dc1aee6864b
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-03 12:02:37 -0300

    feat: Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="William Filho"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsa magni dicta eaque debitis? Laborum nemo quas ab aut maxime earum, aspernatur dolorem veniam esse unde porro voluptatem sit voluptatum."
       />
+
+      <Post author="Grabriel Buzzi" content="Novo Post" />
     </div>
   );
 }
