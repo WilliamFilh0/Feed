5597fe842da423711677ed5785ac9a6704bd066b
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-03 12:19:46 -0300

    feat: Adding application logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from "./Header.module.css";
 
+import IgniteLogo from "../assets/Ignite-log.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
       <strong>Ignite Feed</strong>
+      <img src={IgniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
