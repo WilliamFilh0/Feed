b8a2d8dd76a11ea24181d3e45440fca002cd103c
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-03 10:14:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="William Filho"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsa magni dicta eaque debitis? Laborum nemo quas ab aut maxime earum, aspernatur dolorem veniam esse unde porro voluptatem sit voluptatum."
+      />
+    </div>
+  );
+}
