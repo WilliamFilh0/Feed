b5290c320c70f6926a7829a85e341013a2f99511
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-07 12:25:18 -0300

    feat: Adding sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
@@ -8,12 +10,17 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="William Filho"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsa magni dicta eaque debitis? Laborum nemo quas ab aut maxime earum, aspernatur dolorem veniam esse unde porro voluptatem sit voluptatum."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="William Filho"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsa magni dicta eaque debitis? Laborum nemo quas ab aut maxime earum, aspernatur dolorem veniam esse unde porro voluptatem sit voluptatum."
+          />
 
-      <Post author="Grabriel Buzzi" content="Novo Post" />
+          <Post author="Grabriel Buzzi" content="Novo Post" />
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import IgniteLogo from "../assets/Ignite-log.svg";
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
       <img src={IgniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
