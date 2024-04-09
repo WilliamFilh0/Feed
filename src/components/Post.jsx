1d81b8fc0e3045b1d000bf969cce6841462f6945
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-09 11:24:36 -0300

    feat: Adding HTML structure for Comment Component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -47,6 +48,12 @@ export function Post() {
         <footer>
           <button type="submit">Publicar</button>
         </footer>
+
+        <div className={styles.commentList}>
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
       </form>
     </article>
   );
