a4756388d2f5607bcf3a463127a718730ee0a24c
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-10 13:25:10 -0300

    feat: Implement avatar as a reusable component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/WilliamFilh0.png"
-            alt="Foto do Avatar"
-          />
+          <Avatar src="https://github.com/WilliamFilh0.png" />
           <div className={styles.authorInfo}>
             <strong>William Filho</strong>
             <span>Web Developer</span>
