ab957e7b6772635c68f298aa17f04ce16b9e5653
WilliamFilh0 <williamfilho8908@gmail.com>
2024-04-15 23:35:05 -0300

    feat: Iterating Post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,53 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+// author: { urldoAvatar: "", nsme: "", role:  }
+//published_at: Date
+//content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/WilliamFilh0.png",
+      name: "William Filho",
+      role: "CTO",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      {
+        type: "paragraph",
+        content:
+          " Acabei de subir mais um projeto no meu portifa. É um projeto que  fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare ",
+      },
+
+      { type: "link", content: " jane.design/doctorcare " },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator CTO",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      {
+        type: "paragraph",
+        content:
+          " Acabei de subir mais um projeto no meu portifa. É um projeto que  fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare ",
+      },
+
+      { type: "link", content: " jane.design/doctorcare " },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
+//iteração - repetir alguma coisa. Percoreer o array epra cada posição do array eu vou fazer alguma coisa
+
 export function App() {
   return (
     <div>
@@ -13,12 +60,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="William Filho"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsa magni dicta eaque debitis? Laborum nemo quas ab aut maxime earum, aspernatur dolorem veniam esse unde porro voluptatem sit voluptatum."
-          />
-
-          <Post author="Grabriel Buzzi" content="Novo Post" />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
